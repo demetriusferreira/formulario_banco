2cb80a33511a59398f82e358c5b3018d5588eaea
Técnico em Informática 2022.2 <36127512022.2@senacrjedu.local>
2022-12-16 15:34:19 -0300

    fundo verde - form funcionando

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+//Servidor (com sua porta), nome de usuario no banco, senha, schema
+$conexao = new mysqli("localhost:3306", "root", "", "sextafeira");
+
+if($conexao->connect_errno){
+    echo "tem alguma coisa errada";
+}else {
+    echo "tá tudo conectado redondinho";
+}
+
+?>
